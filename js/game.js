d2e14c85e91108551e06725d66ac49c21588f8a3
Doron Kudra <doron0505@gmail.com>
2025-10-22 15:16:24 +0300

    App skeleton

---
+++ js/game.js
@@ -0,0 +1,128 @@
+'use strict'
+
+var gLevel
+var gBoard = []
+var gEmptyCoords
+var isFirstMove = true
+var gGame
+
+
+function onInit() {
+    setGame()
+    setLevel(4, 2)
+    gBoard = buildBoard()
+    setEmptyCoords(gBoard)
+    renderBoard(gBoard)
+}
+
+function clickedCell(pos) {
+    if (isFirstMove) {
+        setMines(pos)
+        isFirstMove = false
+    }
+    renderCell(pos)
+}
+
+function rightClickedCell(pos) {
+
+}
+
+function setGame() {
+    return {
+        isOn: false,
+        revealedCount: 0,
+        markedCount: 0,
+        secsPassed: 0
+    }
+}
+
+
+function setLevel(size, mines) {
+    gLevel = { size, mines }
+}
+
+function buildBoard() {
+    var board = []
+    board = setMat(gLevel.size, gLevel.size, {
+        minesAroundCount: 0,
+        isRevealed: false,
+        isMine: false,
+        isMarked: false
+    })
+    return board
+}
+
+function setEmptyCoords(board) {
+    gEmptyCoords = {}
+    for (var i = 0; i < board.length * board.length; i++) { // 16
+        gEmptyCoords[i] = { i: parseInt(i / board.length), j: i % board.length } // 0,1,2,3  4,5,6,7  8,9,10,11
+    }
+}
+
+function setMines(firstClickPos) {
+    var totalMines = gLevel.mines
+    delete gEmptyCoords[firstClickPos] // never make first click pos a mine
+    console.log(gEmptyCoords)
+    for (var i = 0; i < totalMines; i++) {
+        var keys = Object.keys(gEmptyCoords)
+        if (keys.length === 0) return
+        var randKey = keys[getRandomInt(0, keys.length)]
+        var randCoords = gEmptyCoords[randKey]
+        gBoard[randCoords.i][randCoords.j].isMine = true
+        console.log('added mine')
+        delete gEmptyCoords[randKey]
+    }
+    revealBoard(translatePos(firstClickPos))
+    console.log(gBoard)
+    renderBoard(gBoard)
+}
+
+function revealBoard(pos){
+    var currCell = gBoard[pos.i][pos.j]
+    currCell.isRevealed = true
+}
+
+function renderBoard(board) {
+    var strHTML = ''
+    for (var i = 0; i < board.length; i++) {
+        strHTML += '<tr>'
+        for (var j = 0; j < board[0].length; j++) {
+            const cell = board[i][j]
+            const className = `cell cell-${i * board.length + j}`
+            if(cell.isMine) return
+            if(!cell.isRevealed){ // cell is revealed with no mines around
+                if(!cell.minesAroundCount){
+                    strHTML += `<td class="${className}">
+                                    <button onClick="clickedCell(${i * board.length + j})"></button>
+                                </td>`
+                }
+                else{
+                    strHTML +=  `<td class="${className}">
+                                    <button onClick="clickedCell(${i * board.length + j})"></button>
+                                    <span>${cell.minesAroundCount}</span>
+                                </td>`
+                }
+
+            }
+            else{
+                strHTML += `<td class="${className}">
+                                <span>${cell.minesAroundCount}</span>
+                            </td>`
+            }
+        }
+        strHTML += '</tr>'
+    }
+    const elContainer = document.querySelector('.game-board')
+    elContainer.innerHTML = strHTML
+}
+
+function renderCell(pos) {
+    // Select the elCell and set the value
+    const elButton = document.querySelector(`.cell-${pos} button`)
+    elButton.remove()
+}
+
+function translatePos(pos) { // from obj to int and int to obj
+    if (typeof (pos) === "number") return { i: parseInt(pos / gBoard.length), j: pos % gBoard.length }
+    return pos.i * gBoard.length + pos.j
+}
---
+++ js/utils.js
@@ -0,0 +1,78 @@
+'use strict'
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function makeId(length = 6) {
+    var txt = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return txt
+}
+
+function setMat(rows, cols, value) {
+    const mat = []
+    for (var i = 0; i < rows; i++) {
+        const row = []
+        for (var j = 0; j < cols; j++) {
+            var inner_value = { ...value}
+            row.push(inner_value)
+        }
+        mat.push(row)
+    }
+    return mat
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF'
+    var color = '#'
+
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
+function isInRange(coord) {
+    if (coord.i < 0) return false
+    if (coord.j > COLS-1) return false
+    if (coord.j < 0) return false
+    if (coord.i > ROWS-1) return false
+    return true
+}
+
+function getSameObjCoordsArr(board,obj){
+	var coordsArr = []
+	for(var i = 0; i < board.length; i++){
+		for(var j = 0; j < board[i].length; j++){
+			var currObj = board[i][j]
+			var isSame = true
+			for(var key in obj){
+				if(currObj[key] !== obj[key]) isSame = false
+			}
+			if(isSame) coordsArr.push({i,j})
+		}
+	}
+	return coordsArr
+}
+
+function getSameStrCoordsArr(board,str){
+	var coordsArr = []
+	for(var i = 0; i < board.length; i++){
+		for(var j = 0; j < board[i].length; j++){
+			if(str === board[i][j]) coordsArr.push({i,j})
+		}
+	}
+	return coordsArr
+}
